9f128997a9deab8c572f63ad471c0e175e176456
Benjamin Thurm <bentrm@posteo.de>
2016-05-04 11:11:48 +0200

    Initial commit

---
+++ src/main/java/de/bentrm/camunda/rest/CustomRestApplication.java
@@ -0,0 +1,26 @@
+package de.bentrm.camunda.rest;
+
+import org.camunda.bpm.engine.rest.impl.CamundaRestResources;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+@ApplicationPath("/")
+public class CustomRestApplication extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> classes = new HashSet<>();
+
+        // add all camunda engine rest resources (or just add those that you actually need).
+        classes.addAll(CamundaRestResources.getResourceClasses());
+
+        // mandatory
+        classes.addAll(CamundaRestResources.getConfigurationClasses());
+
+        return classes;
+    }
+
+}
